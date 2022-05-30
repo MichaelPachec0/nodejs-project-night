6e48cc1cd002fd50dfe0b1441a385aeb590097a6
Jaime <jaime.joseph1991@gmail.com>
2022-05-30 18:06:11 -0400

    added reset button and a stored it in a variable in js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 // Insert Javascript here
+
+//Variable that stores the button
+const button = document.querySelector('#resetbtn')
+
+//Event Listener for button, I'm commenting it out for now until API Implementation
+/*
+button.addEventListener('click', resetGame)
+*/
