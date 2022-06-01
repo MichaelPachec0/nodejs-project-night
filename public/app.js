7094ce5fcf910459a41e9bbd3cefbc2958233895
Joe Dravarol <joedravarol@gmail.com>
2022-06-01 14:06:00 +0100

    Render pokemon cards to the DOM.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,25 @@
 // Insert Javascript here
+
+// Initialize pokemon cards facing down
+function initPokemonCards(pokemonArray) {
+  const container = document.querySelector(".game-page");
+
+  // pokemon array ideally will have 16 elements
+  // this will create 16 grid cells into the DOM
+  pokemonArray.forEach((obj, index) => {
+    // Add ids to individual card so that we have a way to hook up event listener if required
+    let markup = `
+      <div id="card-${index}" class="card hidden">
+        <div class="card-back card-face">
+          <img class="pokeball" src="../poke/poke.png">
+        </div>
+        <div class="card-front card-face">
+          <img class="squirtle dance" src="${obj.pokemonImage}">
+        </div>
+      </div>
+    `;
+
+    // Append individual card to DOM.
+    container.insertAdjacentHTML("beforeend", markup);
+  })
+}
