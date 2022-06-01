b701a5d0ffabdfd67670afe7c949859e93f4c015
Ribbal Hussain <ahmedhussain32000@gmail.com>
2022-06-01 17:55:14 -0500

    Added shuffling functionality. Refactored code for better readability. Issue #4 completed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import {createRequire} from "module";
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 // load up env variables from the .env file
 require('dotenv').config()
@@ -8,18 +8,18 @@ const fs = require('fs');
 const url = require('url');
 const figlet = require('figlet');
 
-import {fileTypes} from "./file.js";
-import { returnData } from "./api.js";
+import { fileTypes } from "./file.js";
+import { _parse } from "./api.js";
 
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     // if protocol is undefined, default to http for now
-    const base = ((req.protocol)? req.protocol : "http") + '://' + req.headers.host + '/'
+    const base = ((req.protocol) ? req.protocol : "http") + '://' + req.headers.host + '/'
     const page = new url.URL(req.url, base).pathname
     const params = new URLSearchParams(page.search)
-    if (page === `/api`){
+    if (page === `/api`) {
         // TODO: handle api here
-        returnData()
+        jsonSend(undefined, params, res)
     } else {
         returnFile(page, res)
     }
@@ -35,10 +35,10 @@ function returnFile(tmpFile, res) {
     // account for index.html being the root page
     let file = (tmpFile === "/") ? "/index.html" : tmpFile
     // if file exists in the public folder, serve it
-    if (fileExists(`public${file}`)){
+    if (fileExists(`public${file}`)) {
         fs.readFile(`public${file}`, (err, data) => handleRes(err, data, res, file, true))
     } else {
-        figlet('404!!', (err, data) => handleRes(err, data, res,file, false));
+        figlet('404!!', (err, data) => handleRes(err, data, res, file, false));
     }
 }
 /**
@@ -62,23 +62,33 @@ function fileExists(path) {
  * @param {boolean} success if the file was found this will be true allowing for the server to serve the file
  * in question
  */
-function handleRes(err, data, res, file, success ){
+function handleRes(err, data, res, file, success) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
-    } else if (success){
+    } else if (success) {
         // the content type to use. should never be undefined
         let cType = fileTypes[file.split('.')[1]] //splits the string into an array of 2 elements, from wherever '.' is and returns the extension
         // just in case it is, set make sure there is a value
-        if (!cType){ cType = "" }
-        res.writeHead(200, {'Content-Type':  cType});
+        if (!cType) { cType = "" }
+        res.writeHead(200, { 'Content-Type': cType });
     } else {
         res.writeHead(404)
     }
     res.write(data);
     res.end();
 }
-console.log(process.env)
-console.log(await returnData())
+
+// DO NOT MERGE
+function jsonSend(err, params, res) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    let cType = fileTypes["json"]
+    res.writeHead(200, { 'Content-Type': cType });
+    _parse(params).then((data) => res.end(data))
+}
 server.listen(8000);
